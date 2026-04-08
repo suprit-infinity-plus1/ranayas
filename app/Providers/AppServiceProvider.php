d359d9b44723c925ec70a30a5a54be864fdc3124
suprit-infinity-plus1 <suprit@infinityplus1.in>
2026-04-08 15:45:41 +0530

    dynamic category and keyword added in appserviceprvider file

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Model\TxnCategory;
+use App\Model\TxnKeyword;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,27 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer('layouts.master', function ($view) {
+            $categories = TxnCategory::where('status', true)->get();
+            $parentCategories = $categories->where('parent_id', 0);
+            
+            $dynamicCategory = "";
+            foreach ($parentCategories as $cat) {
+                // Generate simple frontend list items for the top navigation
+                $url = route('cate', $cat->slug_url);
+                $name = htmlspecialchars($cat->name);
+                $dynamicCategory .= "<li class=\"menu-link\">
+                    <a href=\"{$url}\" class=\"link-title\">
+                        <span class=\"sp-link-title\">{$name}</span>
+                    </a>
+                </li>";
+            }
+
+            $keywords = TxnKeyword::select('keyword')->distinct()->get();
+
+            $view->with('dynamicCategory', $dynamicCategory);
+            $view->with('footerDynamicCategory', $parentCategories);
+            $view->with('keywords', $keywords);
+        });
     }
 }
